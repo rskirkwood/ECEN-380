a229540d0a6b25a23e0b3223a884360051872bc3
rskirkwood <101432150+rskirkwood@users.noreply.github.com>
2022-10-03 15:27:58 -0600

    fixed plotting for task 3

--- Lab 2/ecen380_lab2.m
+++ Lab 2/ecen380_lab2.m
@@ -3,27 +3,17 @@
 C = 1.08e-6;
 R_s = 50;
 R_l = 599;
-t_step = 1/100;
 P = 1/(C*R_s);
-t = -1e-3:1e-6:5e-3;
+t_step = 1e-6;
+t = -1e-3:t_step:2.5e-3;
 
 h = (1/(C*R_s))*exp(-t*(R_s+R_l)/(C*R_s*R_l));
-plot(h);
-xlabel('seconds(ms)');
-ylabel('Volts');
 
+t_0 = 0.001;
+x = rectangularPulse(0, t_0, t); 
+y_c = conv(h,x);
 
-
-
-
-%%% Solving the Equation Symbolically %%%
-
-% syms y_n(t);
-% y_n(t) = dsolve('Dy+(1/R_l+1/R_s)/C*y=0', 'y(0)=1','t');
-% y_n = dsolve('Dy+((1/599+1/50)/(1.08e-6)*y)=0', 'y(0)=1','t');
-
-dif_eq = diff(y,t) + ((1/R_l+1/R_s)/C)*y == 0; 
-con = y(0) == 1;
-y_n = dsolve();
-h_sym = P*y_n*u;
-
+t_c = -0.001:t_step:6e-3;
+plot(t_c(1:3000), y_c(1:3000));
+xlabel('seconds (ms)');
+ylabel('volts');
---
+++ Lab 2/ecen_lab2_sym.m
@@ -0,0 +1,25 @@
+%%% Solving the Equation Symbolically %%%
+
+C = 1.08e-6;
+R_s = 50;
+R_l = 599;
+P = 1/(C*R_s);
+
+syms y(t);
+u = heaviside(t);
+dif_eq = (diff(y,t) + (R_l+R_s)/(R_s*R_l*C)*y) == 0; 
+condition = y(0) == 1;
+y_n = dsolve(dif_eq, condition);
+h_sym = P*y_n*heaviside(t);
+figure(1);
+fplot(h_sym);
+xlim([-0.001 0.005]);
+
+t_0 = .001; 
+x = rectangularPulse(0, t_0, t);
+dt = 1e-6;
+tval = -0.001:dt:0.005;
+y_c = conv(x(tval), h_sym(tval))*dt;
+figure(2);
+fplot(x);
+xlim([-0.001 0.005]);
