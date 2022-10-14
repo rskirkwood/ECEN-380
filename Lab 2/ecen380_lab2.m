74ffd7d369a1ab250e1befe86066ce7cb1e0bfc6
rskirkwood <101432150+rskirkwood@users.noreply.github.com>
2022-10-14 11:00:18 -0600

    finished lab book 2

--- Lab 2/ecen380_lab2.m
+++ Lab 2/ecen380_lab2.m
@@ -11,9 +11,10 @@ h = (1/(C*R_s))*exp(-t*(R_s+R_l)/(C*R_s*R_l));
 
 t_0 = 0.001;
 x = rectangularPulse(0, t_0, t); 
-y_c = conv(h,x);
+y_c = 1e-14*conv(h,x);
 
 t_c = -0.001:t_step:6e-3;
 plot(t_c(1:3000), y_c(1:3000));
+title("Square Wave Graph")
 xlabel('seconds (ms)');
 ylabel('volts');
